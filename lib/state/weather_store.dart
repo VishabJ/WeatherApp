bf2184ce35126b9d5bcc3cc267888d9cbd2c9143
admin <akshaykeesari@intellectsoftwares.com>
2023-08-05 10:02:52 +0530

    Dependices

---
+++ lib/state/weather_store.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'weather_store.g.dart';
+
+class WeatherStore extends _WeatherStoreBase with _$WeatherStore {}
+
+abstract class _WeatherStoreBase with Store {
+  
+}
